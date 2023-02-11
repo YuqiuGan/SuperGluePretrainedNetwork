7de11646fd93ef1c422e7ac8b162372798a9ae0a
YuqiuGan <yg2684@columbia.edu>
2023-02-11 03:49:04 -0500

    implemented superglue+X (X:feature extract method), need further test

--- superglue_match.py
+++ superglue_match.py
@@ -23,6 +23,16 @@ def write_xml(kp, des, score, FName):
     cv_file.release() 
     return True
 
+def read_xml(FName):
+    cv_file = cv2.FileStorage(FName, cv2.FILE_STORAGE_READ)
+    keypoints = cv_file.getNode("superpoint_keypoints").mat()
+    keypoints = torch.from_numpy(keypoints).float().to(device)
+    descriptors = cv_file.getNode("superpoint_descriptors").mat()
+    descriptors = torch.from_numpy(descriptors).float().to(device)
+    scores = cv_file.getNode("superpoint_score").mat()
+    scores = torch.from_numpy(scores).float().to(device)
+    return [keypoints, descriptors, scores]
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='SuperGlue demo',
@@ -111,17 +121,108 @@ if __name__ == '__main__':
     }
     matching = Matching(config).eval().to(device)
     
+    vs = VideoStreamer(opt.input, opt.resize, opt.skip,
+                       opt.image_glob, opt.max_length)
+    frame, ret = vs.next_frame()
+    assert ret, 'Error when reading the first frame (try different --input?)'
+
+    frame_tensor = frame2tensor(frame, device)
     # Create the output directories
     input_dir = Path(opt.input_dir)
     print('Looking for data in directory \"{}\"'.format(input_dir))
     output_dir = Path(opt.output_dir)
     output_dir.mkdir(exist_ok=True, parents=True)
     print('Will write matches to directory \"{}\"'.format(output_dir))
-    feature_dir = os.path.joint(str(input_dir).replace("masked_rgb", "features"), "superpoint_results")
-    frame_list = list(Path(feature_dir).glob("*.xml"))
-    frame_list.sort()
-    for _, frame in enumerate(tqdm(frame_list)):
-        pass
+    feature_dir = os.path.join(str(input_dir).replace("masked_rgb", "features"), "superpoint_results")
+    feature_list = list(Path(feature_dir).glob("*.xml"))
+    # frame_list = list(Path(input_dir).glob("*.png"))
+    feature_list.sort()
+    # frame_list.sort()
+    first_feature = read_xml(feature_list[0])
+    first_frame = frame_tensor
+    last_feature = first_feature
+    last_frame = first_frame
+    keys = ['keypoints', 'scores', 'descriptors']
+    for idx, feature in enumerate(tqdm(feature_list[1:])):
+        current_frame, ret = vs.next_frame()
+        current_frame_tensor = frame2tensor(current_frame, device)
+        current_feature = read_xml(feature)
+        last_data = {
+            'keypoints0': last_feature[0],
+            'descriptors0': last_feature[1],
+            'scores0':last_feature[2],
+            'image0': last_frame
+        }
+        current_data = {
+            'keypoints1': current_feature[0],
+            'descriptors1': current_feature[1],
+            'scores1':current_feature[2],
+            'image1': current_frame_tensor
+        }
+        pred = matching({**last_data, **current_data})
+        kpts0 = last_data['keypoints0'][0].cpu().numpy()
+        kpts1 = current_data['keypoints1'][0].cpu().numpy()
+        matches = pred['matches0'][0].cpu().numpy()
+        confidence = pred['matching_scores0'][0].cpu().numpy()
+
+        valid = matches > -1
+        mkpts0 = kpts0[valid]
+        mkpts1 = kpts1[matches[valid]]
+        color = cm.jet(confidence[valid])
+        text = [
+            'SuperGlue',
+            'Keypoints: {}:{}'.format(len(kpts0), len(kpts1)),
+            'Matches: {}'.format(len(mkpts0))
+        ]
+        k_thresh = matching.superpoint.config['keypoint_threshold']
+        m_thresh = matching.superglue.config['match_threshold']
+        small_text = [
+            'Keypoint Threshold: {:.4f}'.format(k_thresh),
+            'Match Threshold: {:.2f}'.format(m_thresh),
+            'Image Pair: {:06}:{:06}'.format(idx-1, idx),
+        ]
+        out = make_matching_plot_fast(
+            last_frame, frame, kpts0, kpts1, mkpts0, mkpts1, color, text,
+            path=None, show_keypoints=opt.show_keypoints, small_text=small_text)
+        
+        if not opt.no_display:
+            cv2.imshow('SuperGlue matches', out)
+            key = chr(cv2.waitKey(1) & 0xFF)
+            if key == 'q':
+                vs.cleanup()
+                print('Exiting (via q) demo_superglue.py')
+                break
+            elif key == 'n':  # set the current frame as anchor
+                last_data = {k+'0': pred[k+'1'] for k in keys}
+                last_data['image0'] = frame_tensor
+                last_frame = frame
+                last_image_id = (vs.i - 1)
+            elif key in ['e', 'r']:
+                # Increase/decrease keypoint threshold by 10% each keypress.
+                d = 0.1 * (-1 if key == 'e' else 1)
+                matching.superpoint.config['keypoint_threshold'] = min(max(
+                    0.0001, matching.superpoint.config['keypoint_threshold']*(1+d)), 1)
+                print('\nChanged the keypoint threshold to {:.4f}'.format(
+                    matching.superpoint.config['keypoint_threshold']))
+            elif key in ['d', 'f']:
+                # Increase/decrease match threshold by 0.05 each keypress.
+                d = 0.05 * (-1 if key == 'd' else 1)
+                matching.superglue.config['match_threshold'] = min(max(
+                    0.05, matching.superglue.config['match_threshold']+d), .95)
+                print('\nChanged the match threshold to {:.2f}'.format(
+                    matching.superglue.config['match_threshold']))
+            elif key == 'k':
+                opt.show_keypoints = not opt.show_keypoints
+
+        if opt.output_dir is not None:
+            #stem = 'matches_{:06}_{:06}'.format(last_image_id, vs.i-1)
+            stem = 'matches_{:06}_{:06}'.format(idx-1, idx)
+            out_file = str(Path(opt.output_dir, stem + '.png'))
+            print('\nWriting image to {}'.format(out_file))
+            cv2.imwrite(out_file, out)
+
+        last_frame = current_frame_tensor
+        last_feature = current_feature
 
     # need to implement:
     # 1. option to freeze the first frame (T, F)
